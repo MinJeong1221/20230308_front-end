790fc132563a80bc675606082c8be608bba07087
MimJeong1221 <mjeong1998@naver.com>
2023-03-24 17:02:21 +0900

    TEST랑 폴더명 정리

---
+++ TEST01/js/index.js
@@ -0,0 +1,12 @@
+let btnList = document.querySelectorAll(".btns button");
+let slideList = document.querySelector(".slideList");
+let btnSlideList = document.querySelectorAll(".btnSlide");
+
+let index = 0;
+
+btnList.forEach((btn, idx) => {
+  btn.addEventListener("click", function () {
+    index = idx;
+    slideList.style.transform = `translate(${index * -100}%)`;
+  });
+});
