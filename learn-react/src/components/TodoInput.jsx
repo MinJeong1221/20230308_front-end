4647bfe168b0efc63c589d780caeca3875c42c63
MimJeong1221 <mjeong1998@naver.com>
2023-03-29 15:36:24 +0900

    TodoHeader React.memo 사용

--- learn-react/src/components/TodoInput.jsx
+++ learn-react/src/components/TodoInput.jsx
@@ -5,12 +5,11 @@ function TodoInput({ createTodo, onChange }) {
   const inputRef = useRef(null);
 
   const handleSubmit = (e) => {
+    e.preventDefault(); // form의 기본 기능 실행 x.
+    createTodo();
     inputRef.current.focus();
   };
 
-  useEffect(() => {
-    console.log("TodoInput Render");
-  });
   return (
     <div>
       <form onSubmit={handleSubmit}>
