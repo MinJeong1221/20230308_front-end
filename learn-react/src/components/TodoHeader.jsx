2921edf1d79e4995db1858d9c497853d942504fb
MimJeong1221 <mjeong1998@naver.com>
2023-03-29 15:25:57 +0900

    17. useMemo, useCallback, React.memo를 통한 렌더링 성능 최적화

---
+++ learn-react/src/components/TodoHeader.jsx
@@ -0,0 +1,12 @@
+export default function TodoHeader({ undoneCount }) {
+  const dateStr = new Date().toLocaleDateString("ko-KR", {
+    dateStyle: "full",
+  });
+
+  return (
+    <div>
+      <h2>{dateStr}</h2>
+      <p>해야할 일 : {undoneCount}</p>
+    </div>
+  );
+}
