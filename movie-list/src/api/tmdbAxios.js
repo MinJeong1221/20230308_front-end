35c99b3096eb785b0cd125fd8a626d5e6bdd2328
MimJeong1221 <mjeong1998@naver.com>
2023-04-07 17:23:41 +0900

    검색 기능 미완성

--- movie-list/src/api/tmdbAxios.js
+++ movie-list/src/api/tmdbAxios.js
@@ -5,6 +5,7 @@ export const tmdbAxios = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
     api_key: process.env.REACT_APP_TMDB_API_KEY,
+    language: "ko-KR",
   },
 });
 
