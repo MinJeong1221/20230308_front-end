1d8a4871f6edec7e88aec8c53268d191293faa18
MinJeong1221 <mjeong1998@naver.com>
2023-04-10 12:43:11 +0900

    검색 및 필터 기능 완성

--- movie-list/src/api/tmdbAxios.js
+++ movie-list/src/api/tmdbAxios.js
@@ -2,11 +2,11 @@
 import axios from "axios";
 
 export const tmdbAxios = axios.create({
-  baseURL: "https://api.themoviedb.org/3",
-  params: {
-    api_key: process.env.REACT_APP_TMDB_API_KEY,
-    language: "ko-KR",
-  },
+    baseURL: "https://api.themoviedb.org/3",
+    params: {
+        api_key: process.env.REACT_APP_TMDB_API_KEY,
+        // language: "ko-KR",
+    },
 });
 
 console.log(process.env.REACT_APP_TMDB_API_KEY);
--- movie-list/src/components/home/SearchBox.jsx
+++ movie-list/src/components/home/SearchBox.jsx
@@ -1,31 +1,42 @@
 import styled from "styled-components";
 import Button from "../common/button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 function SearchBox() {
-  const [query, setQuery] = useState("");
-  return (
-    <Container>
-      <input type="text" onChange={(e) => setQuery(e.target.value)} />
-      <Button>
-        <Link to={"/search?query=" + query}>검색</Link>
-      </Button>
-    </Container>
-  );
+    const [query, setQuery] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        navigate("/search?query=" + query);
+    };
+
+    return (
+        <Container>
+            <form onSubmit={handleSubmit}>
+                <input type="text" onChange={(e) => setQuery(e.target.value)} />
+                <Button>검색</Button>
+            </form>
+        </Container>
+    );
 }
 
 const Container = styled.div`
-  display: flex;
+    form {
+        display: flex;
+    }
 
-  input {
-    flex: 1;
-    padding: 5px 10px;
-    margin-right: 10px;
-    border-radius: 10px;
-    border: 1px solid #bbb;
-    outline: none;
-  }
+    input {
+        flex: 1;
+        padding: 5px 10px;
+        margin-right: 10px;
+        border-radius: 10px;
+        border: 1px solid #bbb;
+        outline: none;
+    }
+    margin-bottom: 20px;
 `;
 
 export default SearchBox;
